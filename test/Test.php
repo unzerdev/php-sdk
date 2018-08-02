2c74f8b544eff6d5d69299baaac3f76458a4a8cb
Simon Gabriel <simon_gabriel@gmx.de>
2018-08-02 09:36:30 +0200

    [change] (PHPLIB-40) Add basic tests.

---
+++ test/Test.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Description
+ *
+ * @license Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ * @copyright Copyright © 2016-present heidelpay GmbH. All rights reserved.
+ *
+ * @link  http://dev.heidelpay.com/
+ *
+ * @author  Simon Gabriel <development@heidelpay.de>
+ *
+ * @package  heidelpay/${Package}
+ */
+namespace heidelpay\NmgPhpSdk\test;
+
+use heidelpay\NmgPhpSdk\Constants\PaymentType;
+use heidelpay\NmgPhpSdk\Heidelpay;
+use heidelpay\NmgPhpSdk\Payment;
+use heidelpay\NmgPhpSdk\PaymentTypes\Card;
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    const KEY = '123456789';
+    const RETURN_URL = 'returnURL.php';
+
+    /** @var Heidelpay $heidelpay */
+    private $heidelpay;
+
+    protected function setUp()
+    {
+        $this->heidelpay = new Heidelpay(self::KEY, self::RETURN_URL);
+    }
+
+    /**
+     * @test
+     */
+    public function heidelpayObjectShouldHaveGettersAndSettersForProperties()
+    {
+        $this->assertSame(self::KEY, $this->heidelpay->getKey());
+        $this->assertTrue($this->heidelpay->isSandboxMode());
+        $this->assertSame(self::RETURN_URL, $this->heidelpay->getReturnUrl());
+
+        $returnUrl = 'newReturnURL.php';
+        $key = '987654321';
+        $sandboxMode = false;
+
+        $this->heidelpay->setSandboxMode($sandboxMode);
+        $this->heidelpay->setKey($key);
+        $this->heidelpay->setReturnUrl($returnUrl);
+
+        $this->assertSame($key, $this->heidelpay->getKey());
+        $this->assertEquals($sandboxMode, $this->heidelpay->isSandboxMode());
+        $this->assertSame($returnUrl, $this->heidelpay->getReturnUrl());
+
+    }
+
+    /**
+     * @test
+     */
+    public function heidelpayObjectShouldCreatePaymentObject()
+    {
+        $card = new Card();
+
+        /** @var Payment $payment */
+        $payment = $this->heidelpay->createPayment();
+
+        $this->assertInstanceOf(Payment::class, $payment);
+        $this->assertEquals(PaymentType::CARD, $payment->getPaymentType());
+    }
+}
