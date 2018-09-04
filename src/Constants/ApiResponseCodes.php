2f328b0eb19724b2d575a588b3db989ea8d136b9
Simon Gabriel <simon_gabriel@gmx.de>
2018-09-04 10:04:19 +0200

    [change] (PHPLIB-40) Add response code constants.

---
+++ src/Constants/ApiResponseCodes.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Description
+ *
+ * @license Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ * @copyright Copyright © 2016-present heidelpay GmbH. All rights reserved.
+ *
+ * @link  http://dev.heidelpay.com/
+ *
+ * @author  Simon Gabriel <development@heidelpay.de>
+ *
+ * @package  heidelpay/${Package}
+ */
+
+namespace heidelpay\NmgPhpSdk\Constants;
+
+class ApiResponseCodes
+{
+    const SUCCESS_REQUEST_PROCESSED_IN_TEST_MODE = 'API.000.100.112';
+
+    const ERROR_CHARGE_ALREADY_CANCELED = 'API.340.100.014';
+}
