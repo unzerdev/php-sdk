11e576239c10227ce3d74cb8dc8b4bac1203c38f
Simon Gabriel <simon_gabriel@gmx.de>
2018-09-07 12:50:54 +0200

    [change] (PHPLIB-40) Test fetching payment via id.

--- test/GeneralTests.php
+++ test/GeneralTests.php
@@ -13,6 +13,7 @@
  */
 namespace heidelpay\NmgPhpSdk\test;
 
+use heidelpay\NmgPhpSdk\Constants\Currency;
 use heidelpay\NmgPhpSdk\Exceptions\IllegalKeyException;
 use heidelpay\NmgPhpSdk\Heidelpay;
 
@@ -26,6 +27,28 @@ class GeneralTests extends BasePaymentTest
     {
         $this->expectException(IllegalKeyException::class);
         $this->heidelpay = new Heidelpay(BasePaymentTest::PUBLIC_KEY);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldFetchPayment()
+    {
+        $card = $this->heidelpay->createPaymentType($this->createCard());
+        $payment = $this->createPayment();
+        $payment->setPaymentType($card);
+        $payment->authorize(12.0, Currency::EUROPEAN_EURO, self::RETURN_URL);
+        $payment->charge(5.0);
+        $this->assertAmounts($payment, 7, 5, 12, 0);
+
+        $secondPayment = $this->createPayment();
+        $this->assertEmpty($secondPayment->getId());
+        $this->assertAmounts($secondPayment, 0, 0, 0, 0);
+        $this->assertTrue($secondPayment->isPending());
+        $secondPayment->setId($payment->getId());
+        $secondPayment->fetch();
+        $this->assertAmounts($secondPayment, 7, 5, 12, 0);
+        $this->assertTrue($secondPayment->isPartlyPaid());
     }
 
 }
