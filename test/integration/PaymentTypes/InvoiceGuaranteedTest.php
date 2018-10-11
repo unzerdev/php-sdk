d2c2c44b532bc838ac906ad0a099daba8fb7641c
Simon Gabriel <simon_gabriel@gmx.de>
2018-10-11 10:37:37 +0200

    [change] (PHPLIB-40) Add invoice guaranteed tests.

---
+++ test/integration/PaymentTypes/InvoiceGuaranteedTest.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * This class defines integration tests to verify interface and functionality
+ * of the payment method invoice guaranteed.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * @copyright Copyright © 2016-present heidelpay GmbH. All rights reserved.
+ *
+ * @link  http://dev.heidelpay.com/
+ *
+ * @author  Simon Gabriel <development@heidelpay.com>
+ *
+ * @package  heidelpay/mgw_sdk/tests/integration/payment_types
+ */
+namespace heidelpay\MgwPhpSdk\test\integration\PaymentTypes;
+
+use heidelpay\MgwPhpSdk\Constants\Currency;
+use heidelpay\MgwPhpSdk\Resources\PaymentTypes\InvoiceGuaranteed;
+use heidelpay\MgwPhpSdk\test\BasePaymentTest;
+
+class InvoiceGuaranteedTest extends BasePaymentTest
+{
+    /**
+     * Verifies invoice guaranteed payment type can be created.
+     *
+     * @test
+     *
+     * @return InvoiceGuaranteed
+     */
+    public function invoiceGuaranteedTypeShouldBeCreatable(): InvoiceGuaranteed
+    {
+        /** @var InvoiceGuaranteed $invoiceGuaranteed */
+        $invoiceGuaranteed = new InvoiceGuaranteed();
+        $invoiceGuaranteed = $this->heidelpay->createPaymentType($invoiceGuaranteed);
+        $this->assertInstanceOf(InvoiceGuaranteed::class, $invoiceGuaranteed);
+        $this->assertNotNull($invoiceGuaranteed->getId());
+
+        return $invoiceGuaranteed;
+    }
+
+    /**
+     * Verify invoice guaranteed can be shipped.
+     *
+     * @test
+     *
+     * @param InvoiceGuaranteed $invoiceGuaranteed
+     * @depends invoiceGuaranteedTypeShouldBeCreatable
+     */
+    public function verifyInvoiceGuaranteedShipment(InvoiceGuaranteed $invoiceGuaranteed)
+    {
+        $authorization = $invoiceGuaranteed->authorize(
+            100.0,
+            Currency::EUROPEAN_EURO,
+            self::RETURN_URL,
+            $this->getMaximumCustomer()
+        );
+        $this->assertNotNull($authorization);
+        $this->assertNotEmpty($authorization->getId());
+
+        $shipment = $this->heidelpay->ship($authorization->getPayment());
+        $this->assertNotNull($shipment);
+        $this->assertNotEmpty($shipment->getId());
+    }
+
+    /**
+     * Verify that an invoice guaranteed object can be fetched from the api.
+     *
+     * @test
+     *
+     * @param InvoiceGuaranteed $invoiceGuaranteed
+     *
+     * @depends invoiceGuaranteedTypeShouldBeCreatable
+     */
+    public function invoiceGuaranteedTypeCanBeFetched(InvoiceGuaranteed $invoiceGuaranteed)
+    {
+        $fetchedInvoiceGuaranteed = $this->heidelpay->fetchPaymentType($invoiceGuaranteed->getId());
+        $this->assertInstanceOf(InvoiceGuaranteed::class, $fetchedInvoiceGuaranteed);
+        $this->assertEquals($invoiceGuaranteed->getId(), $fetchedInvoiceGuaranteed->getId());
+    }
+}
