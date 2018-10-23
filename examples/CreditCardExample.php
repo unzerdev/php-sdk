5015b592b9f221b308785f7afdd067040693c1ed
Simon Gabriel <simon_gabriel@gmx.de>
2018-10-23 12:22:44 +0200

    [refactor] (PHPLIB-40) Changed example to current state of heidelpayUIExamples.

--- examples/CreditCardExample.php
+++ examples/CreditCardExample.php
@@ -34,99 +34,122 @@
     <title>
         Heidelpay UI Examples
     </title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css" />
-    <link rel="stylesheet" href="https://static.heidelpay.com/v1/heidelpay.css" />
     <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css" />
+
+    <link rel="stylesheet" href="https://static.heidelpay.com/v1/heidelpay.css" />
     <script type="text/javascript" src="https://static.heidelpay.com/v1/heidelpay.js"></script>
+    <style>
+        html, body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            min-width: initial;
+        }
+    </style>
 </head>
 
-<body style="padding: 70px 0 0">
+<body style="width: 100%; height: 330px">
 
-<div class="ui container segment">
-    <div id="dimmer-holder" class="ui active dimmer" style="display: none;">
-        <div class="ui loader"></div>
+    <style>
+        .heidelpaySandboxNotify {
+            display: none !important;
+        }
+    </style>
+    <div class="ui segment">
+        <div id="dimmer-holder" class="ui active dimmer" style="display: none;">
+            <div class="ui loader"></div>
+        </div>
+        <form id="payment-form" class="heidelpayUI form" novalidate>
+            <div class="field">
+                <label for="card-number">Card Number</label>
+                <div class="heidelpayUI left icon input">
+                    <div id="heidelpay-i-card-number" class="heidelpayInput"></div>
+                    <i id="card-icon" class="icon h-iconimg-card-default"></i>
+                </div>
+            </div>
+            <div class="two fields unstackable">
+                <div class="field">
+                    <label for="card-expiry-date">Expiry Date</label>
+                    <div class="heidelpayUI left icon input">
+                        <div id="heidelpay-i-card-expiry" class="heidelpayInput"></div>
+                        <i class="icon h-iconimg-card-expiry"></i>
+                    </div>
+                </div>
+                <div class="field">
+                    <label id="label-card-ccv" for="card-ccv">CVC</label>
+                    <div class="heidelpayUI left icon input">
+                        <div id="heidelpay-i-card-cvc" class="heidelpayInput"></div>
+                        <i class="icon h-iconimg-card-cvc"></i>
+                    </div>
+                </div>
+            </div>
+
+            <p id="error-holder" style="color: #9f3a38"></p>
+            <div class="field">
+                <button class="ui primary button" type="submit">Pay</button>
+            </div>
+        </form>
     </div>
-    <form id="payment-form" class="ui form" novalidate>
-        <div class="field">
-            <label for="example-card-number">Card Number</label>
-            <span id="card-brand"></span>
-            <div id="heidelpay-i-card-number"></div>
-        </div>
-        <div class="two fields">
-            <div class="field">
-                <label for="example-card-expiry-date">Expiry Date</label>
-                <div id="heidelpay-i-card-expiry"></div>
-            </div>
-            <div class="field">
-                <label id="label-card-ccv" for="example-card-ccv">CVC</label>
-                <div id="heidelpay-i-card-cvc"></div>
-            </div>
-        </div>
 
-        <div id="error-holder" class="field" style="color: #9f3a38"></div>
+    <script>
+        var heidelpayObj = new heidelpay('s-pub-2a10fcyD4qVbJGdp76QSoAXoOrO3WrLz');
 
-        <div class="field">
-            <button class="ui primary button" type="submit">Pay</button>
-        </div>
-    </form>
-</div>
+        // Credit Card example
+        var Card = heidelpayObj.Card();
+        Card.create('number', {
+            containerId: 'heidelpay-i-card-number',
+        });
+        Card.create('expiry', {
+            containerId: 'heidelpay-i-card-expiry',
+        });
+        Card.create('cvc', {
+            containerId: 'heidelpay-i-card-cvc',
+        });
 
-<script>
-    var heidelpay = new heidelpay('s-pub-2a10fcyD4qVbJGdp76QSoAXoOrO3WrLz');
+        Card.addEventListener('change', function (e) {
+            if (e.cardType) {
+                let $card = $('#card-icon');
+                $card.removeClass();
+                $card.addClass(`icon h-iconimg-${e.cardType.imgName}`)
+            }
 
-    // Credit Card example
-    var Card = heidelpay.Card()
-    Card.create('number', {
-        containerId: 'heidelpay-i-card-number',
-    });
-    Card.create('expiry', {
-        containerId: 'heidelpay-i-card-expiry',
-    });
-    Card.create('cvc', {
-        containerId: 'heidelpay-i-card-cvc',
-    });
+            // error handling
+            var $inputElement = $(`#heidelpay-i-card-${e.type}`);
+            var $icon = $inputElement.next();
+            var $errorHolder = $('#error-holder');
+            if (e.success === false && e.error) {
+                $inputElement.closest('.heidelpayUI.input').addClass('error');
+                $inputElement.closest('.field').addClass('error');
+                $icon.addClass('h-iconimg-error');
+                $errorHolder.html(e.error)
+            } else if (e.success) {
+                $inputElement.parent('.heidelpayUI.input').removeClass('error');
+                $inputElement.closest('.field').removeClass('error');
+                $icon.removeClass('h-iconimg-error');
+                $errorHolder.html('')
+            }
+        });
 
-    Card.addEventListener('change', function (e) {
-        if (e.cardType) {
-            $('#card-icon').removeClass();
-            $('#card-icon').addClass(`icon h-iconimg-${e.cardType.imgName}`)
-        }
+        // // Handle card form submission.
+        var form = document.getElementById('payment-form');
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
 
-        // error handling
-        var $inputElement = $(`#heidelpay-i-card-${e.type}`)
-        var $icon = $inputElement.next()
-        var $errorHolder = $('#error-holder')
-        if (e.success === false && e.error) {
-            $inputElement.closest('.heidelpayUI.input').addClass('error')
-            $inputElement.closest('.field').addClass('error')
-            $icon.addClass('h-iconimg-error')
-            $errorHolder.html(e.error)
-        } else if (e.success) {
-            $inputElement.parent('.heidelpayUI.input').removeClass('error')
-            $inputElement.closest('.field').removeClass('error')
-            $icon.removeClass('h-iconimg-error')
-            $errorHolder.html('')
-        }
-    });
-
-    // // Handle card form submission.
-    var form = document.getElementById('payment-form');
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-
-        document.getElementById('dimmer-holder').style.display = 'block';
-        Card.createResource()
-            .then(function (data) {
-                document.getElementById('dimmer-holder').innerHTML
-                    = `<div style="color: #eee;top: 43%;position: relative;" class="ui">Resource Id: ${data.id}</div>`
-            })
-            .catch(function (error) {
-                document.getElementById('dimmer-holder').style.display = 'none';
-                document.getElementById('error-holder').innerHTML = error.customerMessage || error.message || 'Error'
-            })
-    });
-</script>
+            document.getElementById('dimmer-holder').style.display = 'block';
+            Card.createResource()
+                .then(function (data) {
+                    document.getElementById('dimmer-holder').innerHTML
+                        = `<div style="color: #eee;top: 43%;position: relative;" class="ui">Resource Id: ${data.id}</div>`
+                })
+                .catch(function (error) {
+                    document.getElementById('dimmer-holder').style.display = 'none';
+                    document.getElementById('error-holder').innerHTML = error.customerMessage || error.message || 'Error'
+                })
+        });
+    </script>
 </body>
 
 </html>
