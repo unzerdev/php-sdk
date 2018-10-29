803eb988200eea19762a1ba21aad89d71c002837
Simon Gabriel <simon_gabriel@gmx.de>
2018-10-29 17:56:48 +0100

    [change] (PHPLIB-52) Travis script: Change path to php-cs-fixer.

--- test/script/travis_script.sh
+++ test/script/travis_script.sh
@@ -4,7 +4,7 @@ set -e
 trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
 
 # run static php-cs-fixer code analysis
-./vendor/bin/php-cs-fixer fix --dry-run --diff --verbose
+../../vendor/bin/php-cs-fixer fix --dry-run --diff --verbose
 
 ## enable xdebug again
 mv ~/.phpenv/versions/$(phpenv version-name)/xdebug.ini.bak ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini
