672f205bc946f7c71892da1191297ef42056998d
Simon Gabriel <simon_gabriel@gmx.de>
2018-10-30 10:59:33 +0100

    [change] (PHPLIB-59) Enable code coverage generation.

--- test/script/travis_script.sh
+++ test/script/travis_script.sh
@@ -10,10 +10,11 @@ trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit
 mv ~/.phpenv/versions/$(phpenv version-name)/xdebug.ini.bak ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini
 
 ## run the tests (todo: enable)
-#./vendor/bin/phpunit test/unit --coverage-xml log/xml/
+# ./vendor/bin/phpunit test/unit --coverage-xml log/xml/
 
 ## perform this task only for php 7 with deps=no
 if [[ ($(phpenv version-name) == "7.0") && ("$deps" == "no") ]]; then
-    echo "Perform integration tests as well";
-    ./vendor/bin/phpunit test/integration
+    echo "Perform integration tests";
+    #./vendor/bin/phpunit test/integration
+    ./vendor/bin/phpunit test/integration --coverage-xml log/xml/
 fi
