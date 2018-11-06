a8b194051f36e6760210a7a3600b10cca1c2af5e
Simon Gabriel <simon_gabriel@gmx.de>
2018-11-06 11:44:51 +0100

    [feature] (PHPLIB-52) Add example for CC with 3D secure.

---
+++ examples/CreditCardWith3D/AuthReversalController.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * This is the controller for the 'Authorization with Reversal' example for Card.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * @copyright Copyright © 2016-present heidelpay GmbH. All rights reserved.
+ *
+ * @link  http://dev.heidelpay.com/
+ *
+ * @author  Simon Gabriel <development@heidelpay.com>
+ *
+ * @package  heidelpay/mgw_sdk/examples
+ */
+
+/** Require the constants of this example */
+require_once __DIR__ . '/Constants.php';
+
+/** Require the composer autoloader file */
+require_once __DIR__ . '/../../../../autoload.php';
+
+use heidelpay\MgwPhpSdk\Constants\Currencies;
+use heidelpay\MgwPhpSdk\Heidelpay;
+
+include '../assets/partials/_controller_php.php';
+
+//#######  1. Catch API and SDK errors, write the message to your log and show the ClientMessage to the client. ########
+try {
+    //#######  2. Create a heidelpay object using your private key #####################################################
+    $heidelpay     = new Heidelpay(EXAMPLE_PRIVATE_KEY);
+
+    //#######  3. Create an authorization. #############################################################################
+    $authorization = $heidelpay->authorize(100.0, Currencies::EURO, $paymentTypeId, AUTH_REVERSAL_CONTROLLER_URL);
+    addSuccess($authorization->getAmount() . ' ' . $authorization->getCurrency() .
+            ' have been authorized for payment ' . $authorization->getPaymentId() . '.');
+
+    //#######  4. Create a reversal for part of the authorized amount. #################################################
+    $reversal = $heidelpay->cancelAuthorizationByPayment($authorization->getPaymentId(), 50.00);
+    addSuccess('The amount of ' . $reversal->getAmount() . ' ' . $authorization->getCurrency() .
+            ' of Authorization ' . $authorization->getId() . ' of payment ' . $authorization->getPaymentId() .
+            ' has been canceled .');
+
+    //#######  5. Charge the full amount of the authorization. #########################################################
+    $charge = $heidelpay->chargeAuthorization($authorization->getPaymentId());
+    addSuccess('The amount of ' . $charge->getAmount() . ' ' . $charge->getCurrency() .
+            ' has been charged for payment ' . $authorization->getPaymentId() . '.');
+
+    //#######  6. Fetch the payment object to get the current state. ###################################################
+    $payment = $charge->getPayment();
+    addInfo('The payment ' . $payment->getId() . ' has the status ' . $payment->getStateName() . '.');
+
+} catch (\heidelpay\MgwPhpSdk\Exceptions\HeidelpayApiException $e) {
+    returnError($e->getClientMessage());
+} catch (\heidelpay\MgwPhpSdk\Exceptions\HeidelpaySdkException $e) {
+    returnError($e->getClientMessage());
+}
+
+returnResponse();
