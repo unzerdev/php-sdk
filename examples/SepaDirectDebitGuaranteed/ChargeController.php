13b4ea41202baddb19113b19d155d58387384716
Simon Gabriel <simon_gabriel@gmx.de>
2018-11-06 15:53:15 +0100

    [feature] (PHPLIB-52) Add example for SepaDirectDebit Guaranteed (unfinished due to problems with the javascript).

---
+++ examples/SepaDirectDebitGuaranteed/ChargeController.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * This is the controller for the 'Charge' transaction example for SepaDirectDebitGuaranteed.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * @copyright Copyright © 2016-present heidelpay GmbH. All rights reserved.
+ *
+ * @link  http://dev.heidelpay.com/
+ *
+ * @author  Simon Gabriel <development@heidelpay.com>
+ *
+ * @package  heidelpay/mgw_sdk/examples
+ */
+
+/** Require the constants of this example */
+require_once __DIR__ . '/Constants.php';
+
+/** Require the composer autoloader file */
+require_once __DIR__ . '/../../../../autoload.php';
+
+use heidelpay\MgwPhpSdk\Constants\Currencies;
+use heidelpay\MgwPhpSdk\Exceptions\HeidelpayApiException;
+use heidelpay\MgwPhpSdk\Exceptions\HeidelpaySdkException;
+use heidelpay\MgwPhpSdk\Heidelpay;
+use heidelpay\MgwPhpSdk\Resources\Customer;
+use heidelpay\MgwPhpSdk\Resources\Payment;
+
+include '../assets/partials/_controller_php.php';
+
+//#######  1. Catch API and SDK errors, write the message to your log and show the ClientMessage to the client. ########
+try {
+    //#######  2. Create a heidelpay object using your private key #####################################################
+    $heidelpay     = new Heidelpay(EXAMPLE_PRIVATE_KEY);
+
+    //#######  3. Create a charge with a new customer. #################################################################
+    $charge = $heidelpay->charge(11.0, Currencies::EURO, $paymentTypeId, RESULT_CONTROLLER_URL, $customer);
+
+} catch (HeidelpayApiException $e) {
+    //#######  4. In case of an error redirect to your failure page. ###################################################
+    redirect(FAILURE_URL);
+} catch (HeidelpaySdkException $e) {
+    redirect(FAILURE_URL);
+}
+
+//#######  5. If everything is fine redirect to your success page. #####################################################
+if (($charge->getPayment() instanceof Payment) && $charge->getRedirectUrl() !== null) {
+    $_SESSION['paymentId'] = $charge->getPaymentId();
+    redirect($charge->getRedirectUrl());
+}
+redirect(FAILURE_URL);
