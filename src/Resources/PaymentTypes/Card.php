64d4a56a71d2878ef682d3b17ebaba1e1e3f8a83
Simon Gabriel <simon_gabriel@gmx.de>
2018-11-14 17:26:06 +0100

    [change] (PHPLIB-67) Add tests to verify Card payment type and fix some special cases.

--- src/Resources/PaymentTypes/Card.php
+++ src/Resources/PaymentTypes/Card.php
@@ -37,6 +37,8 @@ class Card extends BasePaymentType
      *
      * @param string $number
      * @param string $expiryDate
+     *
+     * @throws \RuntimeException
      */
     public function __construct($number, $expiryDate)
     {
@@ -61,6 +63,8 @@ class Card extends BasePaymentType
 
     /** @var string $brand */
     private $brand = '';
+
+    //</editor-fold>
 
     //<editor-fold desc="Getters/Setters">
 
@@ -95,13 +99,20 @@ class Card extends BasePaymentType
      * @param string $expiryDate
      *
      * @return Card
+     *
+     * @throws \RuntimeException
      */
     public function setExpiryDate($expiryDate): Card
     {
         $expiryDateParts = explode('/', $expiryDate);
-        if (\count($expiryDateParts) > 1) {
-            $this->expiryDate = date('m/Y', mktime(0, 0, 0, $expiryDateParts[0], 1, $expiryDateParts[1]));
+        if (\count($expiryDateParts) !== 2 || $expiryDateParts[0] > 12 ||
+            !\is_numeric($expiryDateParts[0]) || !\is_numeric($expiryDateParts[1]) ||
+            !\ctype_digit($expiryDateParts[0]) || !\ctype_digit($expiryDateParts[1]) ||
+            \strlen($expiryDateParts[1]) > 4) {
+            throw new \RuntimeException('Invalid expiry date!');
         }
+        $this->expiryDate = date('m/Y', mktime(0, 0, 0, $expiryDateParts[0], 1, $expiryDateParts[1]));
+
         return $this;
     }
 
@@ -165,6 +176,5 @@ class Card extends BasePaymentType
         return $this;
     }
 
-    //</editor-fold>
     //</editor-fold>
 }
