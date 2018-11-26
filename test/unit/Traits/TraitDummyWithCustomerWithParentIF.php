e44a7405a25a8f690cd2eeb92d3ee83d3e343c50
Simon Gabriel <simon_gabriel@gmx.de>
2018-11-26 12:45:59 +0100

    [change] (PHPLIB-67) CanAuthorizeWithCustomer: Add tests to verify authorize method.

---
+++ test/unit/Traits/TraitDummyWithCustomerWithParentIF.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * This class defines a dummy implementing traits without customer dependency and with implementing the parent
+ * interface.
+ *
+ * Copyright (C) 2018 heidelpay GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @link  http://dev.heidelpay.com/
+ *
+ * @author  Simon Gabriel <development@heidelpay.com>
+ *
+ * @package  heidelpay/mgw_sdk/test/unit
+ */
+namespace heidelpay\MgwPhpSdk\test\unit\Traits;
+
+use heidelpay\MgwPhpSdk\Heidelpay;
+use heidelpay\MgwPhpSdk\Interfaces\HeidelpayParentInterface;
+use heidelpay\MgwPhpSdk\Traits\CanAuthorizeWithCustomer;
+
+class TraitDummyWithCustomerWithParentIF implements HeidelpayParentInterface
+{
+    use CanAuthorizeWithCustomer;
+
+    /**
+     * Returns the heidelpay root object.
+     *
+     * @return Heidelpay
+     *
+     * @throws \RuntimeException
+     */
+    public function getHeidelpayObject(): Heidelpay
+    {
+        return new Heidelpay('s-priv-123');
+    }
+
+    /**
+     * Returns the url string for this resource.
+     *
+     * @param bool $appendId
+     *
+     * @return string
+     */
+    public function getUri($appendId = true): string
+    {
+        return 'test/uri/';
+    }
+}
