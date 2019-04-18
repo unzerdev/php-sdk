09e6e0917495686e202c1a27af3274938c7aa0d1
Simon Gabriel <simon_gabriel@gmx.de>
2019-04-18 17:16:15 +0200

    [change] (PHPLIB-65) Example: Refactore webhook registration.

--- examples/Webhooks/index.php
+++ examples/Webhooks/index.php
@@ -35,22 +35,101 @@ use heidelpayPHP\Exceptions\HeidelpayApiException;
 use heidelpayPHP\Heidelpay;
 use heidelpayPHP\Resources\Webhook;
 
-try {
-    $heidelpay = new Heidelpay(HEIDELPAY_PHP_PAYMENT_API_PRIVATE_KEY);
-    $heidelpay->setDebugMode(true)->setDebugHandler(new ExampleDebugHandler());
-
-    $heidelpay->deleteAllWebhooks();
-
-    $webhooks = $heidelpay->registerMultipleWebhooks(CONTROLLER_URL, [WebhookEvents::ALL]);
-
-    echo 'Events registered: <br>';
-    foreach ($webhooks as $webhook) {
-        /** @var Webhook $webhook */
-        echo print_r($webhook->expose(), 1) . '<br>';
-
-    }
-} catch (HeidelpayApiException $e) {
-    $heidelpay->debugLog('Error: ' . $e->getMessage());
-} catch (RuntimeException $e) {
-    $heidelpay->debugLog('Error: ' . $e->getMessage());
+function printMessage($type, $title, $text)
+{
+    echo '<div class="ui ' . $type . ' message">'.
+            '<div class="header">' . $title . '</div>'.
+            '<p>' . nl2br($text) . '</p>'.
+         '</div>';
 }
+
+function printError($text)
+{
+    printMessage('error', 'Error', $text);
+}
+
+function printSuccess($title, $text)
+{
+    printMessage('success', $title, $text);
+}
+
+function printInfo($title, $text)
+{
+    printMessage('info', $title, $text);
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>
+        Heidelpay UI Examples
+    </title>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"
+            integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css" />
+</head>
+
+<body style="margin: 70px 70px 0;">
+<div class="ui container segment">
+    <h2 class="ui header">
+        <i class="envelope outline icon"></i>
+        <div class="content">
+            Webhook registration
+<!--            <div class="sub header">The following Webhook events </div>-->
+        </div>
+    </h2>
+
+    <?php
+        // Show info message if the general test keys are used
+        if (DEFAULT_PRIVATE_KEY === HEIDELPAY_PHP_PAYMENT_API_PRIVATE_KEY) {
+            printMessage(
+                'yellow',
+                'Attention: You are using the default key pair!',
+                "Keep in mind that webhooks are registered for the private key used to create them.\n" .
+                "This may lead to unwanted behaviour, since someone else using the same key pair might change your webhooks e.g. by deleting them.\n" .
+                'We suggest you replace the predefined key pair in file _enableExamples.php with your own'
+            );
+        }
+
+        try {
+            $heidelpay = new Heidelpay(HEIDELPAY_PHP_PAYMENT_API_PRIVATE_KEY);
+            $heidelpay->setDebugMode(true)->setDebugHandler(new ExampleDebugHandler());
+
+            try {
+                $heidelpay->deleteAllWebhooks();
+                printSuccess(
+                    'De-registered all existing events for this private key',
+                    'Unsubscribed all events registered for the private key: "' . HEIDELPAY_PHP_PAYMENT_API_PRIVATE_KEY . '".'
+                );
+            } catch (HeidelpayApiException $e) {
+            }
+
+            $webhooks = $heidelpay->registerMultipleWebhooks(CONTROLLER_URL, [WebhookEvents::ALL]);
+
+            foreach ($webhooks as $webhook) {
+                /** @var Webhook $webhook */
+                printSuccess(
+                    'Event registered',
+                    '<strong>Event:</strong> ' . $webhook->getEvent() . '</br>' .
+                    '<strong>URL:</strong> ' . $webhook->getUrl() . '</br>'
+                );
+            }
+
+        } catch (HeidelpayApiException $e) {
+            printError($e->getMessage());
+            $heidelpay->debugLog('Error: ' . $e->getMessage());
+        } catch (RuntimeException $e) {
+            printError($e->getMessage());
+            $heidelpay->debugLog('Error: ' . $e->getMessage());
+        }
+    ?>
+
+</div>
+</body>
+
+</html>
