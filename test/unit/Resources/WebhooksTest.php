d2e7170fa52c02b27fb49baef0306414f91e3094
Simon Gabriel <simon_gabriel@gmx.de>
2019-04-26 16:59:38 +0200

    [cleanup] Webhooks add unit test to verify handling of multiple webhooks in response.

--- test/unit/Resources/WebhooksTest.php
+++ test/unit/Resources/WebhooksTest.php
@@ -25,9 +25,13 @@
 namespace heidelpayPHP\test\unit\Resources;
 
 use heidelpayPHP\Constants\WebhookEvents;
+use heidelpayPHP\Heidelpay;
+use heidelpayPHP\Resources\Webhook;
 use heidelpayPHP\Resources\Webhooks;
 use heidelpayPHP\test\BaseUnitTest;
 use PHPUnit\Framework\Exception;
+use RuntimeException;
+use stdClass;
 
 class WebhooksTest extends BaseUnitTest
 {
@@ -40,10 +44,10 @@ class WebhooksTest extends BaseUnitTest
      */
     public function mandatoryConstructorParametersShouldDefaultToEmptyString()
     {
-        $webhook = new Webhooks();
-        $this->assertEquals('', $webhook->getUrl());
-        $this->assertIsEmptyArray($webhook->getEventList());
-        $this->assertIsEmptyArray($webhook->getWebhookList());
+        $webhooks = new Webhooks();
+        $this->assertEquals('', $webhooks->getUrl());
+        $this->assertIsEmptyArray($webhooks->getEventList());
+        $this->assertIsEmptyArray($webhooks->getWebhookList());
     }
 
     /**
@@ -74,12 +78,60 @@ class WebhooksTest extends BaseUnitTest
      */
     public function adderOfWebhookEventsOnlyAllowsValidEvents()
     {
-        $webhook = new Webhooks('https://dev.heidelpay.com', []);
-        $this->assertIsEmptyArray($webhook->getEventList());
+        $webhooks = new Webhooks('https://dev.heidelpay.com', []);
+        $this->assertIsEmptyArray($webhooks->getEventList());
 
-        $webhook->setUrl('https://docs.heidelpay.com');
-        $webhook->addEvent('invalidEvent');
-        $this->assertEquals('https://docs.heidelpay.com', $webhook->getUrl());
-        $this->assertIsEmptyArray($webhook->getEventList());
+        $webhooks->setUrl('https://docs.heidelpay.com');
+        $webhooks->addEvent('invalidEvent');
+        $this->assertEquals('https://docs.heidelpay.com', $webhooks->getUrl());
+        $this->assertIsEmptyArray($webhooks->getEventList());
+    }
+
+    /**
+     * Verify response handling.
+     *
+     * @test
+     *
+     * @throws Exception
+     * @throws RuntimeException
+     */
+    public function responseHandlingShouldBehaveAsExpected()
+    {
+        $webhooks = new Webhooks('https://dev.heidelpay.com', [WebhookEvents::CHARGE, WebhookEvents::AUTHORIZE]);
+        $webhooks->setParentResource(new Heidelpay('s-priv-123'));
+        $this->assertEquals('https://dev.heidelpay.com', $webhooks->getUrl());
+        $this->assertEquals([WebhookEvents::CHARGE, WebhookEvents::AUTHORIZE], $webhooks->getEventList());
+
+        $response = new stdClass();
+        $eventA = new stdClass();
+        $eventA->id = 's-whk-1084';
+        $eventA->url = 'https://dev.heidelpay.com';
+        $eventA->event = 'charge';
+        $eventB = new stdClass();
+        $eventB->id = 's-whk-1085';
+        $eventB->url = 'https://dev.heidelpay.com';
+        $eventB->event = 'authorize';
+        $events = [$eventA, $eventB];
+
+        $response->events = $events;
+
+        $webhooks->handleResponse($response);
+        $webhookList = $webhooks->getWebhookList();
+        $this->assertCount(2, $webhookList);
+        /**
+         * @var Webhook $webhookA
+         * @var Webhook $webhookB
+         */
+        list($webhookA, $webhookB) = $webhookList;
+        $this->assertInstanceOf(Webhook::class, $webhookA);
+        $this->assertInstanceOf(Webhook::class, $webhookB);
+        $this->assertEquals(
+            ['event' => 'charge', 'id' => 's-whk-1084', 'url' => 'https://dev.heidelpay.com'],
+            $webhookA->expose()
+        );
+        $this->assertEquals(
+            ['event' => 'authorize', 'id' => 's-whk-1085', 'url' => 'https://dev.heidelpay.com'],
+            $webhookB->expose()
+        );
     }
 }
