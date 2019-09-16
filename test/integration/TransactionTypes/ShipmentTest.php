17709984c35a6067e821cc9888ee97e7222ff810
sixer1182 <simon.gabriel@gmx.de>
2019-09-16 09:27:32 +0200

    [change] (PHPLIB-238) Ship: Add optional parameter orderId.

--- test/integration/TransactionTypes/ShipmentTest.php
+++ test/integration/TransactionTypes/ShipmentTest.php
@@ -52,7 +52,7 @@ class ShipmentTest extends BasePaymentTest
         $this->assertNotNull($charge->getId());
         $this->assertNotNull($charge);
 
-        $shipment = $this->heidelpay->ship($charge->getPayment(), $this->generateRandomId());
+        $shipment = $this->heidelpay->ship($charge->getPayment(), $this->generateRandomId(), $this->generateRandomId());
         $this->assertNotNull($shipment->getId());
         $this->assertNotNull($shipment);
 
@@ -81,7 +81,7 @@ class ShipmentTest extends BasePaymentTest
         );
 
         $payment  = $charge->getPayment();
-        $shipment = $payment->ship($this->generateRandomId());
+        $shipment = $payment->ship($this->generateRandomId(), $this->generateRandomId());
         $this->assertNotNull($shipment);
         $this->assertNotEmpty($shipment->getId());
         $this->assertNotEmpty($shipment->getUniqueId());
@@ -112,7 +112,7 @@ class ShipmentTest extends BasePaymentTest
         );
 
         $payment  = $charge->getPayment();
-        $shipment = $this->heidelpay->ship($payment, $this->generateRandomId());
+        $shipment = $this->heidelpay->ship($payment, $this->generateRandomId(), $this->generateRandomId());
         $this->assertNotNull($shipment->getId());
         $this->assertNotNull($shipment);
     }
@@ -137,7 +137,7 @@ class ShipmentTest extends BasePaymentTest
         );
 
         $payment  = $charge->getPayment();
-        $shipment = $this->heidelpay->ship($payment, $this->generateRandomId());
+        $shipment = $this->heidelpay->ship($payment, $this->generateRandomId(), $this->generateRandomId());
         $this->assertSuccess($shipment);
     }
 }
