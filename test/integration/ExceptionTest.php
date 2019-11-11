a014e9b1be83f09378b26d89624471de776a8e46
sixer1182 <simon.gabriel@gmx.de>
2019-11-11 16:39:45 +0100

    [cleanup] Fix review issue.
    Signed-off-by: sixer1182 <simon.gabriel@gmx.de>

--- test/integration/ExceptionTest.php
+++ test/integration/ExceptionTest.php
@@ -45,8 +45,8 @@ class ExceptionTest extends BasePaymentTest
     public function apiExceptionShouldHoldClientMessage()
     {
         $giropay             = $this->heidelpay->createPaymentType(new Giropay());
+        $firstClientMessage  = '';
         $secondClientMessage = '';
-        $firstClientMessage  = $secondClientMessage;
 
         try {
             $this->heidelpay->authorize(1.0, 'EUR', $giropay, self::RETURN_URL);
