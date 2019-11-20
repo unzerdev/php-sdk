d6bafbcdf37cab2bedbc829f55b8a09374da517b
sixer1182 <simon.gabriel@gmx.de>
2019-11-20 20:05:48 +0100

    [change] (PHPLIB-XX) Refactor travis tests.

--- test/script/travis_script.sh
+++ test/script/travis_script.sh
@@ -9,7 +9,7 @@ trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit
 ## enable xdebug again
 mv ~/.phpenv/versions/$(phpenv version-name)/xdebug.ini.bak ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini
 
-echo "Env Dev: $dev";
+echo "Env Dev: $deps";
 echo "Env Level: $level";
 
 ## run the unit tests
