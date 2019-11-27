8e5cea26b8d8e3724f0e6922c5630bd35dd589e2
sixer1182 <simon.gabriel@gmx.de>
2019-11-27 11:24:33 +0100

    [refactor] (PHPLIB-274) Change file headers of service interfaces.

--- src/Interfaces/ResourceServiceInterface.php
+++ src/Interfaces/ResourceServiceInterface.php
@@ -1,15 +1,26 @@
 <?php
 /**
- * Description
+ * The interface for the ResourceService.
  *
- * @license Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
- * @copyright Copyright © 2016-present heidelpay GmbH. All rights reserved.
+ * Copyright (C) 2019 heidelpay GmbH
  *
- * @link  http://dev.heidelpay.com/
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * @author  Simon Gabriel <development@heidelpay.de>
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * @package  heidelpay/${Package}
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @link  https://docs.heidelpay.com/
+ *
+ * @author  Simon Gabriel <development@heidelpay.com>
+ *
+ * @package  heidelpayPHP\Interfaces
  */
 namespace heidelpayPHP\Interfaces;
 
