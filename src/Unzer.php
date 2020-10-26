1ee750955b7daaca1be2c7fb224977d764e5c905
David.Owusu <david.owusu@unzer.com>
2020-10-26 11:27:49 +0100

    [change] (SDK-247) rebranding: reset SDK version.

--- src/Unzer.php
+++ src/Unzer.php
@@ -63,7 +63,7 @@ class Unzer implements UnzerParentInterface, PaymentServiceInterface, ResourceSe
     public const BASE_URL = 'api.heidelpay.com';
     public const API_VERSION = 'v1';
     public const SDK_TYPE = 'heidelpayPHP';
-    public const SDK_VERSION = '1.2.9.0';
+    public const SDK_VERSION = '1.0.0.0';
 
     /** @var string $key */
     private $key;
