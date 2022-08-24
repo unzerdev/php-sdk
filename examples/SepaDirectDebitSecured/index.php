e9dfb20ef7772556f1fd73c248405ad1a0ee97b8
David.Owusu <david.owusu@unzer.com>
2022-08-24 17:30:01 +0200

    (UMCS-518) Adjust example layouts - Place all pay-buttons into a div with "field" class. - ensure that all payment forms use correct css classes.

--- examples/SepaDirectDebitSecured/index.php
+++ examples/SepaDirectDebitSecured/index.php
@@ -46,7 +46,7 @@ require_once __DIR__ . '/../../../../autoload.php';
 
 <p><a href="https://docs.unzer.com/reference/test-data" target="_blank">Click here to open our test data in new tab.</a></p>
 
-<form id="payment-form">
+<form id="payment-form" class="unzerUI form">
     <div id="sepa-secured-IBAN" class="field">
         <!-- The IBAN field UI Element will be inserted here -->
     </div>
@@ -54,7 +54,9 @@ require_once __DIR__ . '/../../../../autoload.php';
         <!-- The customer form UI element will be inserted here -->
     </div>
     <div class="field" id="error-holder" style="color: #9f3a38"></div>
-    <button class="unzerUI primary button fluid" id="submit-button" type="submit">Pay</button>
+    <div class="field">
+        <button class="unzerUI primary button fluid" id="submit-button" type="submit">Pay</button>
+    </div>
 </form>
 
 <script>
