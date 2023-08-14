148eadad43dc271133d4035e32a82ee273d859bc
David.Owusu <david.owusu@unzer.com>
2023-08-14 17:34:36 +0200

    [CC-516] Add payU example.

---
+++ examples/PayU/Controller.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * This is the controller for the PayU example.
+ * It is called when the pay button on the index page is clicked.
+ *
+ * Copyright (C) 2020 - today Unzer E-Com GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @link  https://docs.unzer.com/
+ *
+ * @package  UnzerSDK\examples
+ */
+
+/** Require the constants of this example */
+require_once __DIR__ . '/Constants.php';
+
+/** @noinspection PhpIncludeInspection */
+/** Require the composer autoloader file */
+require_once __DIR__ . '/../../../../autoload.php';
+
+use UnzerSDK\examples\ExampleDebugHandler;
+use UnzerSDK\Exceptions\UnzerApiException;
+use UnzerSDK\Unzer;
+
+session_start();
+session_unset();
+
+$clientMessage = 'Something went wrong. Please try again later.';
+$merchantMessage = 'Something went wrong. Please try again later.';
+
+function redirect($url, $merchantMessage = '', $clientMessage = '')
+{
+    $_SESSION['merchantMessage'] = $merchantMessage;
+    $_SESSION['clientMessage']   = $clientMessage;
+    header('Location: ' . $url);
+    die();
+}
+
+// You will need the id of the payment type created in the frontend (index.php)
+if (!isset($_POST['resourceId'])) {
+    redirect(FAILURE_URL, 'Resource id is missing!', $clientMessage);
+}
+$paymentTypeId   = $_POST['resourceId'];
+
+// Catch API errors, write the message to your log and show the ClientMessage to the client.
+try {
+    // Create an Unzer object using your private key and register a debug handler if you want to.
+    $unzer = new Unzer(UNZER_PAPI_PRIVATE_KEY);
+    $unzer->setDebugMode(true)->setDebugHandler(new ExampleDebugHandler());
+
+    // Create a charge transaction to get the redirectUrl.
+    $transaction = new \UnzerSDK\Resources\TransactionTypes\Charge(12.32, 'PLN', RETURN_CONTROLLER_URL);
+    $unzer->performCharge($transaction, $paymentTypeId);
+
+    // You'll need to remember the paymentId for later in the ReturnController
+    $_SESSION['PaymentId'] = $transaction->getPaymentId();
+    $_SESSION['ShortId']   = $transaction->getShortId();
+
+    // Redirect to the PayU page
+    if (!$transaction->isError() && $transaction->getRedirectUrl() !== null) {
+        redirect($transaction->getRedirectUrl());
+    }
+
+    // Check the result message of the charge to find out what went wrong.
+    $merchantMessage = $transaction->getMessage()->getCustomer();
+} catch (UnzerApiException $e) {
+    $merchantMessage = $e->getMerchantMessage();
+    $clientMessage = $e->getClientMessage();
+} catch (RuntimeException $e) {
+    $merchantMessage = $e->getMessage();
+}
+redirect(FAILURE_URL, $merchantMessage, $clientMessage);
