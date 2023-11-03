5aa0c5cf26815b169bc7e42c18131ec4502532fd
David.Owusu <david.owusu@unzer.com>
2023-11-03 10:59:23 +0100

    [CC-584 Release-PHP-SDK-3] Adjust paylater invoice cancel test because partial reversal is now supported by API.

--- test/integration/TransactionTypes/PaylaterCancelTest.php
+++ test/integration/TransactionTypes/PaylaterCancelTest.php
@@ -72,13 +72,14 @@ class PaylaterCancelTest extends BaseIntegrationTest
     /**
      * @test
      */
-    public function verifyPartReversalAttemptWillRaiseApiException(): void
+    public function verifyPartReversalIsPossible(): void
     {
         $authorization = $this->createPaylaterInvoiceAuthorization();
         $reversalAmount = 33.33;
-        $this->expectException(UnzerApiException::class);
+        $this->assertAmounts($authorization->getPayment(), 99.99, 0, 99.99, 0);
 
         $this->unzer->cancelAuthorizedPayment($authorization->getPayment(), new Cancellation($reversalAmount));
+        $this->assertAmounts($authorization->getPayment(), 66.66, 0, 66.66, 0);
     }
 
     /**
