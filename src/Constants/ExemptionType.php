8f26a952d1c8d98f611a4f4dd022bfbab99c4570
david.owusu <david.owusu@unzer.com>
2024-04-29 10:01:15 +0200

    [CC-897] Add constant for tra (transaction risk analysis).

--- src/Constants/ExemptionType.php
+++ src/Constants/ExemptionType.php
@@ -4,11 +4,9 @@ namespace UnzerSDK\Constants;
 
 /**
  * This file contains valid exemption type values
- *
- * @link  https://dev.unzer.com/
- *
  */
 class ExemptionType
 {
     public const LOW_VALUE_PAYMENT    = 'lvp';
+    public const TRANSACTION_RISK_ANALYSIS    = 'tra';
 }
