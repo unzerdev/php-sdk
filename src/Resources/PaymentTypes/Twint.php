93a3c480c470cdeebefb4c063e09af8a5a9c5ca3
Patrick Weinstein <patrick.weinstein@patworx.de>
2024-08-20 10:15:20 +0200

    TWINT: CanDirectCharge Trait

--- src/Resources/PaymentTypes/Twint.php
+++ src/Resources/PaymentTypes/Twint.php
@@ -3,8 +3,10 @@
 namespace UnzerSDK\Resources\PaymentTypes;
 
 use UnzerSDK\Traits\HasAccountInformation;
+use UnzerSDK\Traits\CanDirectCharge;
 
 class Twint extends BasePaymentType
 {
     use HasAccountInformation;
+    use CanDirectCharge;
 }
