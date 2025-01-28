ec1489cefd44fa51d7bd987e1d5e3c347aae3049
david.owusu <david.owusu@unzer.com>
2025-01-28 15:29:26 +0100

    [CC-1935 release_3-12-0] Prepare release 3.12.0

--- src/Unzer.php
+++ src/Unzer.php
@@ -57,7 +57,7 @@ class Unzer implements
     public const BASE_URL = 'api.unzer.com';
     public const API_VERSION = 'v1';
     public const SDK_TYPE = 'UnzerPHP';
-    public const SDK_VERSION = '3.11.0';
+    public const SDK_VERSION = '3.12.0';
 
     /** @var string $key */
     private $key;
